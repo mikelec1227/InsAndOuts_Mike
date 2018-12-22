939110c14fd6fde0b17ad4a62d2fd74d11fe0bfc
Mike Lecolant <mikelec1227@gmail.com>
2018-12-22 18:21:32 -0500

    Update Final Project

---
+++ Final_Project/final_project/final_project.ino
@@ -0,0 +1,103 @@
+/* Mike Lecolant
+Home Alarm System
+The sensor inside the box can sense movement up to 200 centimeters. Once movement is
+detected, the piezo buzzer and light strip will go off, detering any would be intruder
+from entering your home
+Can also be used for a fun greeting in the doorway for non intruders
+
+Pieces Used to build:
+Arduino
+Breadboard
+USB External Power Suppl and 12v Power Adapter
+RGB LED Strip Light
+MOSFET Transistors
+Ultrasonic Ping Sensor
+Piezo Buzzer
+ */
+
+
+#include <NewPing.h> //Libraries I had to download because the code initially didnt work. 
+#include <NewTone.h> 
+ 
+
+#define BLUE_PIN  13
+#define GREEN_PIN  12 
+#define RED_PIN   11
+
+ 
+#define TRIGGER  6
+#define ECHO  7
+#define DISTANCE 100 //Distance in centimeters for the sensor
+
+NewPing sonar(TRIGGER, ECHO, DISTANCE);
+boolean triggered = false;
+
+ 
+ 
+#define ALARM 4
+float sinVal;
+int toneVal;
+ 
+void setup(){
+   
+ //Pins for the LED lights
+   pinMode(RED_PIN,OUTPUT);
+   pinMode(BLUE_PIN,OUTPUT);
+   pinMode(GREEN_PIN,OUTPUT);
+   
+   pinMode(ALARM, OUTPUT);
+ 
+   //Resetting the lights
+   analogWrite(RED_PIN,0);
+   analogWrite(BLUE_PIN,0);
+   analogWrite(GREEN_PIN,0);
+ 
+delay(50);
+Serial.begin(115200);
+ 
+ 
+}
+ 
+void loop(){
+    if(triggered == true){
+      alarm();
+    }
+    else{
+      delay(50);
+      unsigned int uS = sonar.ping(); 
+      unsigned int distance = uS / US_ROUNDTRIP_CM;
+      Serial.println(distance);
+      if(distance < 100){
+         triggered = true;
+      }
+   }
+}
+ 
+void alarm(){
+   color(255,0,0);
+   delay(25);
+   color(0,255,0);
+   delay(25);
+   color(0,0,255);
+   delay(25);
+   color(255,255,0);
+   delay(25);
+
+ 
+   for (int x=0; x<180; x++) {
+    sinVal = (sin(x*(3.1412/180)));      
+    toneVal = 2000+(int(sinVal*1000));    
+    NewTone(ALARM, toneVal);   
+
+     /*Had to do alot of research on this part because I wasnt too familiar with the codes for piezo
+      buzzers. 
+      */            
+  }
+}
+ 
+void color (unsigned char red, unsigned char green, unsigned char blue)
+{    
+    analogWrite(RED_PIN, red);    
+    analogWrite(BLUE_PIN, blue);
+    analogWrite(GREEN_PIN, green);
+}
